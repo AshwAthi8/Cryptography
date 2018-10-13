b23c70c682b85c2c7afb9340b9c2f50d8d617fb0
ais2397 <35501599+ais2397@users.noreply.github.com>
2018-10-13 15:51:26 +0530

    Update padding.py

--- padding.py
+++ padding.py
@@ -1,10 +1,10 @@
-s=input()
+s=input() #the string which has to be padded
 x=len(s)
 c=x%16
-r=16-c
+r=16-c	#hex of this number will be used to pad the string.
 for i in range (0,r,1):
 	s=s+"\\"+hex(r)
-print(s)
+print(s) #the padded string.
 
 
 
